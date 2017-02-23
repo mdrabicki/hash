eb59c6c06a2e824b18a8cbeffcd93a10ef374f38
Maciek <mdrabicki@outlook.com>
2017-02-23 18:35:15 +0100

    init

---
+++ src/task1/mainTest.java
@@ -0,0 +1,14 @@
+package task1;
+
+import org.junit.Assert;
+
+public class mainTest {
+
+    @org.junit.Test
+    public void multiply() throws Exception {
+
+        Assert.assertEquals(2, main.multiply(1, 2));
+
+    }
+
+}
